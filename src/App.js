a7fcc2f12aa129eee1936ff36b3f75777f370787
Renan Frutuozo <frutuozo29@gmail.com>
2019-03-27 20:12:35 -0300

    Antd

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
 import { ReactComponent as Logo } from "./assets/logo.svg";
+import Button from "antd/lib/button";
 import "./App.css";
+import Sider from "./common/header/index";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <Sider />
           <Logo />
+          <br />
+          <Button type="primary">Button</Button>
         </header>
       </div>
     );
