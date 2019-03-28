616340f4bde6a3c7fdb82c5f29d6a81dd1f54542
Renan Frutuozo <frutuozo29@gmail.com>
2019-03-27 21:05:55 -0300

    Veiaco layout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React, { Component } from "react";
-import { ReactComponent as Logo } from "./assets/logo.svg";
-import Button from "antd/lib/button";
 import "./App.css";
-import Sider from "./common/header/index";
+import Main from "./common/main/index";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <Sider />
-          <Logo />
-          <br />
-          <Button type="primary">Button</Button>
-        </header>
+        <Main />
       </div>
     );
   }
---
+++ src/common/main/index.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import { Layout, Menu, Breadcrumb, Icon } from "antd";
+const { Header, Content, Footer, Sider } = Layout;
+const SubMenu = Menu.SubMenu;
+
+class Main extends Component {
+  state = {
+    collapsed: false
+  };
+  onCollapse = collapsed => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  };
+  render() {
+    return (
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sider
+          collapsible
+          collapsed={this.state.collapsed}
+          onCollapse={this.onCollapse}
+        >
+          <div className="logo" />
+          <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+            <Menu.Item key="1" className="menuitem">
+              <Icon type="pie-chart" />
+              <span>Cadastros</span>
+            </Menu.Item>
+            <Menu.Item key="2" className="menuitem">
+              <Icon type="desktop" />
+              <span>Movimentação</span>
+            </Menu.Item>
+            <SubMenu
+              key="sub1"
+              title={
+                <span>
+                  <Icon type="user" />
+                  <span>Usuário</span>
+                </span>
+              }
+              className="menuitem"
+            >
+              <Menu.Item key="3">Tom</Menu.Item>
+              <Menu.Item key="4">Bill</Menu.Item>
+              <Menu.Item key="5">Alex</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="9" className="menuitem">
+              <Icon type="file" />
+              <span>Configurações</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: "#fff", padding: 0 }} />
+          <Content style={{ margin: "0 16px" }}>
+            <Breadcrumb style={{ margin: "16px 0" }} className="menuitem">
+              <Breadcrumb.Item>Home</Breadcrumb.Item>
+              <Breadcrumb.Item>Cadastro</Breadcrumb.Item>
+            </Breadcrumb>
+            <div style={{ padding: 24, background: "#fff", minHeight: 360 }}>
+              Veiaco front-end
+            </div>
+          </Content>
+          <Footer style={{ textAlign: "center" }}>Renan Frutuozo ©2019</Footer>
+        </Layout>
+      </Layout>
+    );
+  }
+}
+
+export default Main;
