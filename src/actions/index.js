867fedce081dfb74dd38d6d8b3c4f05b68e602b5
Renan Frutuozo <frutuozo29@gmail.com>
2019-04-24 21:01:14 -0300

    Set Url production

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,3 @@
-export const apiBaseUrl = 'http://localhost:1000'
+const { NODE_ENV } = process.env
+
+export const apiBaseUrl = NODE_ENV !== 'production' ? 'http://localhost:1000' : 'http://veiaco-app.herokuapp.com'
