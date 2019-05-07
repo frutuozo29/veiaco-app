849bf2499b7f8598dd5b0687664e0ae81719af71
Renan Frutuozo <frutuozo29@gmail.com>
2019-05-07 09:12:19 -0300

    Mudando para https

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,3 @@
 const { NODE_ENV } = process.env
 
-export const apiBaseUrl = NODE_ENV !== 'production' ? 'http://localhost:1000' : 'http://veiaco-app.herokuapp.com'
+export const apiBaseUrl = NODE_ENV !== 'production' ? 'http://localhost:1000' : 'https://veiaco-app.herokuapp.com'
