d11e0336f4aede91ef7331e4646992d148ec1a8a
Renan Frutuozo <frutuozo29@gmail.com>
2020-02-04 23:10:53 -0300

    Context

--- src/components/shared/RadioGroup/index.js
+++ src/components/shared/RadioGroup/index.js
@@ -3,24 +3,29 @@ import React, { memo } from 'react'
 // prop-types
 import PropTypes from 'prop-types'
 
+// Radio Group Context
+import RadioGroupContext from './RadioGroupContext'
 
 // styles
 import { Container, ContentChildren, Label } from './styles'
 
-export const RadioGroup = ({ label, children }) => {
+export const RadioGroup = ({ label, value, children, onChange }) => {
 
   return (
-    <Container >
-      <Label>{label}</Label>
-      <ContentChildren total={children.length}>
-        {children}
-      </ContentChildren>
-    </Container>
+    <RadioGroupContext.Provider value={{ onChange }}>
+      <Container>
+        <Label>{label}</Label>
+        <ContentChildren total={children.length}>
+          {children}
+        </ContentChildren>
+      </Container>
+    </RadioGroupContext.Provider>
   )
 }
 
 RadioGroup.propTypes = {
   label: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
   children: PropTypes.arrayOf(PropTypes.element).isRequired
 }
 
